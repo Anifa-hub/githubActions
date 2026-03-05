87eab215a3de8c1542b350869c3d1957fbc7dcf6
anifa <anifabazubagira@gmail.com>
2026-03-05 10:40:22 +0200

    feat:automated sort feature created && configure the pipeline

---
+++ src/test/java/Sort.java
@@ -0,0 +1,19 @@
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Sort {
+    public static void main(String[] args){
+        Page page = Playwright.create().chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        ).newPage();
+
+        page.navigate("https://practicesoftwaretesting.com/");
+        Locator sort = page.locator("//select[@aria-label='sort']");
+        sort.selectOption("name,asc");
+        
+        page.close();
+
+    }
+}
